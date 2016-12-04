ad031efa76d685bcdf28afa371918c9f3d0b5a4d
John.N.Y <john.n.y@icloud.com>
2016-12-04 17:49:21 +0800

    stanford swap re-code

--- ptr/book.figs/ptr.swap.cc
+++ ptr/book.figs/ptr.swap.cc
@@ -8,7 +8,7 @@ This program illustrates the use of call by reference to exchagne the value of t
 using namespace std;
 
 /*function prototype */
-void swap(int& x, int& y);
+void swap(int* x, int* y);
 
 /* main program */
 int main(){
@@ -17,7 +17,7 @@ int main(){
     cin >> n1;
     cout << "Enter n2; ";
     cin >> n2;
-    if (n1>n2) swap(n1,n2);
+    if (n1>n2) swap(&n1,&n2);
     cout << "the range is " << n1 << " to " << n2 << "." << endl;
     return 0;
 }
@@ -28,7 +28,7 @@ usage: swap(x,y)
 -------------------
 exchage the value of x and y. the arguments are passed by reference and can therefore by modified.
 */
-void swap(int& x, int& y){
+void swap(int* x, int* y){
     int tmp = x;
     x = y;
     y = tmp;
